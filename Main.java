f15432987338a2247920376e7f61541549721007
Aimear <zion.hanley@outlook.com>
2026-07-14 00:02:21 -0400

    TODO added

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
         while (true) {
             gameState.update();
         }
+        // TODO: Find out how to change the console encoding to support the chess characters.
         // TODO: Make it so a piece cant pass through another piece.
         // TODO: Make it so a piece cant move to a tile with a piece of the same color.
         // TODO: Make it so a piece can capture a piece of the opposite color.
